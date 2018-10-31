5c6bf56f334f01bd784a7515ce4619639e7d888a
Matthew DeBlock <vastencn@gmail.com>
2018-10-31 19:00:14 +0800

    starting php config files

--- php/config.php
+++ php/config.php
@@ -4,18 +4,17 @@
 ********* php implementation v 0.1 beta ************
 ****************************************************
 **************************** by : Matthew DeBlock **
+*************************************** NOV 2018 ***
 ****************************************************
 
 
 ***************************************************/
 
+$operating_path  = "/var/www/html/";
+$hashpute_path   = "hashpute/";
 
 
-
-function pute(){
-
-  return 1;
-  }
-
+require($operating_path.$hashpute_path."hash.php");
+require($operating_path.$hashpute_path."pute.php");
 
 ?>
--- php/hash.php
+++ php/hash.php
@@ -1,3 +1,41 @@
 <?php
+/***************************************************
+************* Hashputer processor ******************
+********* php implementation v 0.1 beta ************
+****************************************************
+**************************** by : Matthew DeBlock **
+*************************************** NOV 2018 ***
+****************************************************
+
+
+***************************************************/
+
+
+function hp_hash_config($arg_ar){
+  $config_ar=array();
+
+// First byte is config byte
+//
+// Bit 1-3
+//    Size of hash val (num bytes: 1,2,3,4,5,6,7,8)
+//
+// Bit 4
+//    Mask 1 (yes/no)
+//
+// Bit 5
+//    Mask 2 (yes/no)
+//
+// Bit 7
+//    Bit offset (yes/no)
+//
+// Bit 8
+//    Length restrict (yes/no)
+
+
+  return $config_ar;
+  }
+
+function hp_hash(){
+  }
 
 ?>
